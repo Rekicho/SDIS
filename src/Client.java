27f3a9c38922b06dd38209b4c5bce09b25c568e7
Rekicho <up201604145@fe.up.pt>
2019-02-28 16:02:45 +0000

    Completed client

--- src/Client.java
+++ src/Client.java
@@ -1,24 +1,41 @@
-import java.io.*;
 import java.net.*;
 
 public class Client {
+    private static final String REGISTER = "register";
+    private static final String LOOKUP = "lookup";
 
-    public static void main(String args[]) throws Exception
-    {
-        BufferedReader inFromUser =
-                new BufferedReader(new InputStreamReader(System.in));
+    private static final String ALREADY_REGISTERED = "-1";
+    private static final String NOT_FOUND = "NOT_FOUND";
+    private static final String ERROR = "ERROR";
+
+    public static void main(String[] args) throws Exception {
+        if ((args.length == 4 && args[2].equals(LOOKUP)) || (args.length == 5 && args[2].equals(REGISTER))) {
+            System.out.println("Usage: java Client <host_name> <port_number> <oper> <opnd>*");
+            System.out.println("       java Client <host_name> <port_number> register <plate number> <owner name>");
+            System.out.println("       java Client <host_name> <port_number> lookup <plate number>");
+            System.exit(-1);
+        }
+
         DatagramSocket clientSocket = new DatagramSocket();
-        InetAddress IPAddress = InetAddress.getByName("localhost");
-        byte[] sendData = new byte[1024];
-        byte[] receiveData = new byte[1024];
-        String sentence = inFromUser.readLine();
-        sendData = sentence.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+        InetAddress IPAddress = InetAddress.getByName(args[0]);
+
+        byte[] sendData, receiveData = new byte[1024];
+
+        String request = args[2].equals(LOOKUP) ?
+                         args[2].toUpperCase() + " " + args[3] :
+                         args[2].toUpperCase() + " " + args[3] + " " + args[4];
+        sendData = request.getBytes();
+
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, Integer.parseInt(args[1]));
         clientSocket.send(sendPacket);
+
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
         clientSocket.receive(receivePacket);
-        String modifiedSentence = new String(receivePacket.getData());
-        System.out.println("FROM SERVER:" + modifiedSentence);
+
+        String response = new String(receivePacket.getData()).trim();
+
+        System.out.println(request + ": " + (response.equals(ALREADY_REGISTERED) || response.equals(NOT_FOUND) ? ERROR : response));
+
         clientSocket.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,91 +1,87 @@
-import java.io.*;
 import java.net.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Server {
+    private static final String REGISTER = "REGISTER";
+    private static final String LOOKUP = "LOOKUP";
 
-    private static String REGISTER = "REGISTER";
-    private static String LOOKUP = "LOOKUP";
-    private static Map plates = new HashMap();
+    private static final String PLATE_REGEX = "[0-9A-Z]{2}-[0-9A-Z]{2}-[0-9A-Z]{2}";
 
-    public static void main(String args[]) throws Exception
-    {
-        if(args.length != 1){
-            // Error
+    private static Map<String, String> plates = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage: java Server <port_number>");
+            System.exit(-1);
         }
 
         DatagramSocket serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
-        while(true)
-        {
-            byte[] receiveData = new byte[1024];
-            byte[] sendData = new byte[1024];
+
+        while (true) {
+            byte[] receiveData = new byte[1024], sendData;
+
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
-            String sentence = new String( receivePacket.getData());
-            System.out.println("RECEIVED: " + sentence);
-            sentence = sentence.trim();
-            String answer = parseRequest(sentence);
+
+            String answer = parseRequest(new String(receivePacket.getData()).trim());
 
             InetAddress IPAddress = receivePacket.getAddress();
             int port = receivePacket.getPort();
             sendData = answer.getBytes();
-            DatagramPacket sendPacket =
-                    new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
             serverSocket.send(sendPacket);
         }
     }
 
-    private static String parseRequest(String request){
-        String args[] = request.split(" ",3);
-        if(!isValidPlate(args[1])){
+    private static String parseRequest(String request) {
+        String[] args = request.split(" ", 3);
+        if (!isValidPlate(args[1])) {
             return "INVALID PLATE FORMAT";
         }
 
-        if(args[0].equals(REGISTER)){
-            //Register
-            if(args.length != 3){
+        if (args[0].equals(REGISTER)) {
+            System.out.println("register " + args[1] + " " + args[2]);
+            if (args.length != 3) {
                 return "INVALID N OF ARGS, EXPECTED: REGISTER <plate number> <owner name>";
             }
 
-            return registerPlate(args[1],args[2]);
-        }else if(args[0].equals(LOOKUP)){
-            //Lookup
-            if(args.length != 2){
+            return registerPlate(args[1], args[2]);
+        } else if (args[0].equals(LOOKUP)) {
+            System.out.println("lookup " + args[1]);
+            if (args.length != 2) {
                 return "INVALID N OF ARGS, EXPECTED: LOOKUP <plate number>";
             }
 
             return lookupPlate(args[1]);
-        }else{
+        } else {
             return "INVALID REQUEST\nREGISTER <plate number> <owner name>\nLOOKUP <plate number>";
         }
     }
 
-    private static String registerPlate(String plateNumber, String ownerName){
+    private static String registerPlate(String plateNumber, String ownerName) {
         boolean hasPlate = plates.containsKey(plateNumber);
-        if(hasPlate){
+
+        if (hasPlate) {
             return "-1";
-        }else{
-            plates.put(plateNumber,ownerName);
-            System.out.println("Saved " + plateNumber + " with " + ownerName);
+        } else {
+            plates.put(plateNumber, ownerName);
             return String.valueOf(plates.size());
         }
     }
 
-    private static String lookupPlate(String plateNumber){
+    private static String lookupPlate(String plateNumber) {
         boolean hasPlate = plates.containsKey(plateNumber);
 
-        if(!hasPlate){
-            return "-1";
-        }else{
-            String name = plates.get(plateNumber).toString();
+        if (!hasPlate) {
+            return "NOT_FOUND";
+        } else {
+            String name = plates.get(plateNumber);
             return plateNumber + " " + name;
         }
     }
 
-    private static boolean isValidPlate(String plate){
-        return plate.matches("[0-9A-Z]{2}-[0-9A-Z]{2}-[0-9A-Z]{2}");
+    private static boolean isValidPlate(String plate) {
+        return plate.matches(PLATE_REGEX);
     }
-
-
 }
