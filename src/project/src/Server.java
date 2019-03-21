55cacd7c04f2b90262bfb27d1e8391dc96d866e1
Rekicho <up201604145@fe.up.pt>
2019-03-21 15:33:30 +0000

    Send PUTCHUNK message

--- src/project/src/Server.java
+++ src/project/src/Server.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,10 +16,22 @@ import java.security.MessageDigest;
 public class Server implements ServerRMI {
     private String version;
     private int id;
+    private MulticastSocket mdb;
+    private String mdb_host;
+    private int mdb_port;
 
-    private Server(String version, int id) {
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    private static final int TTL = 1;
+
+    private Server(String version, int id, String mdb_host, int mdb_port) throws Exception {
         this.version = version;
         this.id = id;
+        this.mdb_host = mdb_host;
+        this.mdb_port = mdb_port;
+
+        mdb = new MulticastSocket(mdb_port);
+        mdb.setTimeToLive(TTL);
+        mdb.joinGroup(InetAddress.getByName(mdb_host));
     }
 
     private String header(String message_type, String fileId, int chunkNo, Integer replicationDeg) {
@@ -30,8 +45,16 @@ public class Server implements ServerRMI {
 
 
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] info = (file.getName()+attr.creationTime()+attr.lastModifiedTime()+attr.size()).getBytes();
-            return new String(digest.digest(info));
+            byte[] info = digest.digest((file.getName()+attr.creationTime()+attr.lastModifiedTime()+attr.size()).getBytes());
+
+            char[] hexChars = new char[info.length*2];
+            for ( int j = 0; j < info.length; j++ ) {
+                int v = info[j] & 0xFF;
+                hexChars[j * 2] = hexArray[v >>> 4];
+                hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+            }
+
+            return new String(hexChars);
         } catch (Exception e) {
             return file.getName();
         }
@@ -56,11 +79,21 @@ public class Server implements ServerRMI {
         int count;
         int chunkNo = 0;
         byte[] buffer = new byte[64000];
+        byte[] header;
 
         try {
             do {
                 count = fileToBackup.read(buffer);
-                System.out.println(header("PUTCHUNK", fileId,chunkNo,Integer.parseInt(args[1])));
+                header = header("PUTCHUNK", fileId,chunkNo,Integer.parseInt(args[1])).getBytes();
+
+                byte[] message = new byte[header.length + buffer.length];
+                System.arraycopy(header,0,message,0,header.length);
+                System.arraycopy(buffer,0,message,header.length,buffer.length);
+
+                DatagramPacket chunkPacket = new DatagramPacket(message,message.length,InetAddress.getByName(mdb_host),mdb_port);
+                mdb.send(chunkPacket);
+
+                System.out.println(chunkNo);
                 chunkNo++;
             } while (count == 64000);
         } catch (Exception e) {
@@ -71,13 +104,13 @@ public class Server implements ServerRMI {
     }
 
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Usage: java Server <protocol_version> <server_id> <remote_object_name>");
+        if (args.length != 5) {
+            System.out.println("Usage: java Server <protocol_version> <server_id> <remote_object_name> <MDB_IP> <MDB_port>");
             System.exit(-1);
         }
 
         try {
-            Server obj = new Server(args[0], Integer.parseInt(args[1]));
+            Server obj = new Server(args[0], Integer.parseInt(args[1]), args[3], Integer.parseInt(args[4]));
             ServerRMI stub = (ServerRMI) UnicastRemoteObject.exportObject(obj, 0);
 
             // Bind the remote object's stub in the registry
